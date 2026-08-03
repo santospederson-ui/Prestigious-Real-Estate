bf8bf3a5686acf2e1b5caaff39fe802902aedee0
Stanley Atare <santospederson@gmail.com>
2026-08-03 08:24:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -47,9 +47,17 @@ def get_db_connection():
 # ==========================================
 # CLOUDINARY
 # ==========================================
+
 CLOUDINARY_CLOUD_NAME="da8y4zqz5"
 CLOUDINARY_API_KEY="551545451643298"
 CLOUDINARY_API_SECRET="CtN8D84Db81NFkhUwGUm8W2cvEU"
+
+
+cloudinary.config(
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET
+)
 
 
 
